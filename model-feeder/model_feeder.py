70405040614d89766350eff43c23615ef6cf5050
samuel-backstrom <sbackstrom95@gmail.com>
2020-08-19 10:12:08 +0200

    Implemented model-feeder

--- model-feeder/model_feeder.py
+++ model-feeder/model_feeder.py
@@ -1,11 +1,3 @@
-"""
-    ModelFeeder Todo List
-     1. Read the JSON data 
-     2. Load events into window
-     3. Convert events using the category_encoders lib
-     4. Write window to json?
-"""
-
 import json
 import numpy as np
 from pprint import pprint
@@ -19,33 +11,57 @@ I = 1
 PATH_TO_MOCK_DATA = "mock-dataset/mock_data.json"
 WINDOW_SIZE = 30 * I
 
+def convert_list_of_dicts(lod, keylist):
+    return [[row[key] for key in keylist] for row in lod]
+
+def reshape_events(events):
+    for i in range(len(events)):
+        e = events[i]
+        events[i] = [e[i:i+3] for i in range(0, len(e), 3)]
+    return events
+
+""" Sort events according to timestamp so that they can be put into the neural networks """
 def sort_events(data):
     data.sort(key=lambda e: e['timestamp'])
     pass
 
-def convert_list_of_dicts(lod, keylist):
-    return [[row[key] for key in keylist] for row in lod]
-
+""" Transforms the labeled data using the OneHotEncoder from scikit """
 def encode(data):
-    # Add a variable that indicates time difference from previous data point?
-    # Change timestamp variable
-    X = np.stack(data) # Convert to numpy array
-    Y = np.delete(X, 3, axis=1)
+    X = np.stack(data)
+    Y = list()
+    for i in range(len(X)):
+        Y.append(X[i][0])
+
     enc = OneHotEncoder()
-    transformed_data = enc.fit_transform(Y).toarray() # Results in 333 "input nodes", without timestamp, 58 "input nodes"
-    inversed_data = enc.inverse_transform(transformed_data)
-    pprint(transformed_data)
-    pprint(inversed_data)
-    pass
+    transformed_data = enc.fit_transform(Y).toarray()
+
+    for i in range(len(X)):
+        X[i][0] = transformed_data[i].tolist()
+
+    return X
 
 def read_into_window(data, index):
-    pass    
+    window = list()
+    
+    start = index * WINDOW_SIZE
+    end = start + WINDOW_SIZE
 
-def read_JSON_file(path):
-    pass
+    for i in range(start, end):
+        window.append(data[i])
+    return window
 
 with open(Path(PATH_TO_MOCK_DATA)) as json_file:
     data = json.load(json_file)
     sort_events(data)
+
+    """ Format the data so that it can be handled by the OneHotEncoder """
     events = convert_list_of_dicts(data, ['event', 'sensor', 'sensor_group', 'timestamp'])
-    encode(events)
+    reshaped_events = reshape_events(events)
+
+    """ Encode events """
+    encoded_events = encode(reshaped_events)
+    
+    """ Read a number of events into a window, starting at 0, that will be used as input for the neural networks """
+    index = 0
+    window = read_into_window(encoded_events, index)
+
